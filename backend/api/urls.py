2dd0048aaa5591b3912dede39fb031376209758f
Krivov Aleksandr <krivov.av91@gmail.com>
2023-09-26 10:54:06 +0700

    rename api

---
+++ backend/api/urls.py
@@ -0,0 +1,27 @@
+from django.urls import include, path
+
+from rest_framework import routers
+from rest_framework.routers import DefaultRouter
+from .views import (RecipeViewSet,
+                    IngredientViewSet,
+                    TagViewSet,
+                    CustomUserViewSet)
+
+
+app_name = 'api'
+router = routers.DefaultRouter()
+router = DefaultRouter()
+
+
+router.register('recipes', RecipeViewSet, basename='recipes')
+router.register('ingredients', IngredientViewSet, basename='ingredients')
+router.register('tags', TagViewSet, basename='tags')
+router.register('users', CustomUserViewSet, basename='users')
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+
+]
